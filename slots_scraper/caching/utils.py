bfeb83fe9e66ff99bfe58dc817a2b6b4ceff2ec4
ag-bart <bartag@op.pl>
2024-02-28 12:38:21 +0100

    add extension to param cache key

--- slots_scraper/caching/utils.py
+++ slots_scraper/caching/utils.py
@@ -6,5 +6,5 @@ def cache_key_from_url(url: str, pattern=r'^/([a-zA-Z0-9-]+)/'):
     path = urlparse(url).path
     match = re.match(pattern, path)
     if match:
-        return match.group(1)
+        return f"{match.group(1)}.json"
     return None
